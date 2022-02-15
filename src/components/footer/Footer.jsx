cb6343ef96ce029a6170720c2030235e81a8ae70
Abderraouf <abderraouf.drine99@gmail.com>
2022-02-15 22:48:31 +0300

    disabled eslint

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './footer.css'
 import { FaFacebookSquare, FaTwitterSquare, FaPinterestSquare, FaInstagramSquare } from 'react-icons/fa';
